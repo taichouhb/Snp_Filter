0b07ebc62c0f24a843e96da89abd09fee67da72b
taichouhb <bcheung@umass.edu>
2016-08-30 00:47:03 -0400

    Switched to using Quantmod

--- filter.R
+++ filter.R
@@ -1,28 +1,11 @@
-library(tseries)
+
 library(quantmod)
-library(XML)
 
 
-startDate <- "2005-01-01"
-tables <- readHTMLTable("http://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-#oddly this is not working for me. I get failed to laod error
-tickers <- as.matrix(tables[[1]]["Ticker symbol"])
-
-instrumentRtn <- function(instrument=instrument,startDate=startDate,lag=lag){
-  price <- get.hist.quote(instrument, quote="Adj", start=startDate, retclass="zoo")
-  monthlyPrice <- aggregate(price, as.yearmon, tail, 1)
-  monthlyReturn <- diff(log(monthlyPrice),lag=lag)
-  monthlyReturn <- exp(monthlyReturn)-1
-  return(monthlyReturn)
-}
-
-dataFactor <- list()
-dataRtn <- list()
-
-for (i in 1:length(tickers)) {
-  print(tickers[i])
-  dataFactor[[i]] <- instrumentRtn(tickers[i],startDate,lag=12)
-  dataRtn[[i]] <- instrumentRtn(tickers[i],startDate,lag=1)
-  
-  
+new_env <- new.env()
+symbols <- c("YHOO", "GOOG")
+retrieved_symbols <- getSymbols(symbols, env=new_env, warnings = FALSE)
+ll <- eapply(new_env, function(x) x)  
+for (s in ll){
+  print(s)
 }
